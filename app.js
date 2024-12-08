c714165b2c46a5e79ebd9c18847efc3ed962f90f
Khayam Ijaz <ijazkhayam@gmail.com>
2024-12-08 17:06:17 +0500

    update

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 const input1 = document.querySelector(".toChk");
 const input2 = document.querySelector(".input2");
 const validator = document.querySelector(".validator");
-const toShow = document.querySelector(".toshow")
-const tohide = document.querySelector(".tohide")
+const toShow = document.querySelector(".toshow");
+const tohide = document.querySelector(".tohide");
 
 setInterval(() => {
-    if (input1.value == "") {
-      input2.style.opacity = 0;
-    } 
+  if (input1.value == "") {
+    input2.style.opacity = 0;
+
+  }
 }, 100);
 
 validator.addEventListener("click", (e) => {
+  if (input1.value) {
+    tohide.style.display = "none";
+    toShow.style.display = "block";
+  }
+  setTimeout(() => {
     if (input1.value) {
-        tohide.style.display = "none";
-        toShow.style.display = "block";
+      input2.style.opacity = 1;
     }
-    setTimeout(() => {
-        if (input1.value) {
-          input2.style.opacity = 1;
-        }
-        tohide.style.display = "block";
-        toShow.style.display = "none";
-    }, 1000);
+    tohide.style.display = "block";
+    toShow.style.display = "none";
+  }, 1000);
 });
